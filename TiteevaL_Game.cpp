0fab078546aa35a827fa53d83684f87c9cdc4fd0
larisa-titeeva <82133984+larisa-titeeva@users.noreply.github.com>
2021-05-26 22:55:26 +0500

    с ускорением по оси OY

--- TiteevaL_Game.cpp
+++ TiteevaL_Game.cpp
@@ -1,61 +1,72 @@
 #include "TXLib.h"
 
 void BackgroundDraw();
-void BallDraw (int x, int y, int vx, int vy, int dt, COLORREF OutLine, COLORREF BallColor);
+void BallDraw();
 
 int main()
     {
     txCreateWindow (1000, 600);
 
     BackgroundDraw();
-    BallDraw (200, 300, 5, 6, 1, TX_PINK, TX_RED);
+
+    BallDraw();
 
     return 0;
     }
 
 void BackgroundDraw()
     {
-    txSetColor     (TX_BLUE, 5);
+    txSetColor (TX_BLUE, 5);
     txSetFillColor (TX_BLACK);
 
     txRectangle (100, 100, 900, 500);
+
     }
 
-void BallDraw (int x, int y, int vx, int vy, int dt, COLORREF OutLine, COLORREF BallColor)
+void BallDraw()
     {
-    while (!txGetAsyncKeyState (VK_ESCAPE))
+    int x = 200,  y = 300,
+       vx =   4, vy =   3,
+       ax =   0, ay =   1,
+        r =  10;
+
+    int dt = 1;
+
+    txSetColor     (TX_PINK);
+    txSetFillColor (TX_RED);
+    while  (!txGetAsyncKeyState (VK_ESCAPE))
+    {txCircle (x, y, r);
+
+    x  += vx * dt;
+    y  += vy * dt;
+
+    vx += ax * dt;
+    vy += ay * dt;
+
+    if (x > (900 - r))
         {
-        txSetColor     (OutLine, 3);
-        txSetFillColor (BallColor);
-
-        txCircle (x, y, 20);
-
-        x += vx * dt;
-        y += vy * dt;
-
-        if (x > 880)
-            {
-            vx = -vx;
-            x  = 1760 - x;
-            }
-
-        if (y > 480)
-            {
-            vy = -vy;
-            y  = 960 - y;
-            }
-
-        if (x < 120)
-            {
-            vx = -vx;
-            x  = 240 - x;
-            }
-
-        if (y < 120)
-            {
-            vy = -vy;
-            y  = 240 - y;
-            }
-        txSleep (10);
+        vx = -vx;
+        x  = 2 * (900 - r) - x;
         }
+
+    if (y > (500 - r))
+        {
+        vy = -vy;
+        y  = 2 * (500 - r) - y;
+        }
+
+    if (x < (100 + r))
+        {
+        vx = -vx;
+        x  = 2 * (100 + r) - x;
+        }
+
+    if (y < (100 + r))
+        {
+        vy = -vy;
+        y  = 2 * (100 + r) - y;
+        }
+    txSleep (10);
     }
+    }
+
